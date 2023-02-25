4d9b6cd44869a668b47685ab84622c6612c9a87f
TotallyNotTG <118136211+TotallyNotTG@users.noreply.github.com>
2023-02-25 12:23:46 -0800

    Add files via upload

---
+++ games.tgblox.com/v1.game-recommended.js
@@ -0,0 +1,17 @@
+document.write('<div class="sections">');
+document.write('                <h2>Continue</h2>');
+document.write('                <img src="images.tgblox.com/game-card.png" class="game-card">');
+document.write('                <img src="https://tr.rbxcdn.com/e94c9b4523f7fdef1c925792b55fc036/150/150/Image/Png" class="game-card">');
+document.write('                <h2>Most Popular</h2>');
+document.write('                <img src="images.tgblox.com/game-card.png" class="game-card">');
+document.write('                <img src="https://tr.rbxcdn.com/9b78e73ecff59b92a0d23e0cf4b40292/150/150/Image/Jpeg" class="game-card">');
+document.write('                <img src="https://tr.rbxcdn.com/96aa74618ebbb98a7b7eee77f6978f6f/150/150/Image/Jpeg" class="game-card">');
+document.write('                <img src="https://tr.rbxcdn.com/16e942aa752ccd409894864ab3181ac2/150/150/Image/Jpeg" class="game-card">');
+document.write('                <img src="https://tr.rbxcdn.com/c447e1d5be8d04f06a55066c70407ba0/150/150/Image/Jpeg" class="game-card">');
+document.write('                <h2>Friends Played Most</h2>');
+document.write('                <img src="images.tgblox.com/game-card.png" class="game-card">');
+document.write('                <h2>Recommended For You</h2>');
+document.write('                <img src="images.tgblox.com/game-card.png" class="game-card">');
+document.write('                <h2>Most Engaging</h2>');
+document.write('                <img src="images.tgblox.com/game-card.png" class="game-card">');
+document.write('            </div>');
