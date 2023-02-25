672f561930373af45ba95d48b725a5c38eedc8e1
TotallyNotTG <118136211+TotallyNotTG@users.noreply.github.com>
2023-02-25 12:24:16 -0800

    Add files via upload

---
+++ friends.tgblox.com/friend-list.v1=.js
@@ -0,0 +1,18 @@
+
+document.write('            <p class="friends-error">Unable to load friends.</p>');
+document.write('            <img class="friends-pfp loading" src="images.tgblox.com/freind1.png">');
+document.write('            <img class="friends-pfp loading" src="images.tgblox.com/friend4.png">');
+document.write('            <img class="friends-pfp loading" src="images.tgblox.com/friend2.png">');
+document.write('            <img class="friends-pfp loading" src="images.tgblox.com/friend3.png">');
+document.write('            <img class="friends-pfp loading" src="tr.tgnblx.com/broken-icon2.svg">');
+document.write('            <img class="friends-pfp loading" src="tr.tgnblx.com/broken-icon2.svg">');
+document.write('            <img class="friends-pfp loading" src="tr.tgnblx.com/broken-icon2.svg">');
+document.write('            <img class="friends-pfp loading" src="tr.tgnblx.com/broken-icon2.svg">');
+//
+
+document.write('            <style>');
+document.write('                .friends-error {');
+document.write('                     display: none;');
+document.write('              }');
+document.write('            </style>');
+
