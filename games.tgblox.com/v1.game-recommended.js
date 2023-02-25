2691daf95953165d5dd1f61b2ebed1b9e0ced24e
TotallyNotTG <118136211+TotallyNotTG@users.noreply.github.com>
2023-02-25 12:34:31 -0800

    Update v1.game-recommended.js

--- games.tgblox.com/v1.game-recommended.js
+++ games.tgblox.com/v1.game-recommended.js
@@ -1,6 +1,6 @@
 document.write('<div class="sections">');
 document.write('                <h2>Continue</h2>');
-document.write('                <img src="images.tgblox.com/game-card.png" class="game-card">');
+document.write('                <a href="https://totallynottg.github.io/web.tgblox/Olliehaven-RP"><img src="images.tgblox.com/game-card.png" class="game-card"></a>');
 document.write('                <img src="https://tr.rbxcdn.com/e94c9b4523f7fdef1c925792b55fc036/150/150/Image/Png" class="game-card">');
 document.write('                <h2>Most Popular</h2>');
 document.write('                <img src="images.tgblox.com/game-card.png" class="game-card">');
@@ -10,6 +10,8 @@ document.write('                <img src="https://tr.rbxcdn.com/16e942aa752ccd40
 document.write('                <img src="https://tr.rbxcdn.com/c447e1d5be8d04f06a55066c70407ba0/150/150/Image/Jpeg" class="game-card">');
 document.write('                <h2>Friends Played Most</h2>');
 document.write('                <img src="images.tgblox.com/game-card.png" class="game-card">');
+document.write('                <h2>Events</h2>');
+document.write('                <a href="https://totallynottg.github.io/web.tgblox/pls-donate-eventid=301"><img src="https://tr.rbxcdn.com/d9297b09371f3b7097357d69b2c23907/150/150/Image/Png" class="game-card"></a>');
 document.write('                <h2>Recommended For You</h2>');
 document.write('                <img src="images.tgblox.com/game-card.png" class="game-card">');
 document.write('                <h2>Most Engaging</h2>');
