92bf28f09becb825b92ddc8869b7c15fe2963f5a
TotallyNotTG <118136211+TotallyNotTG@users.noreply.github.com>
2023-02-27 14:46:09 -0800

    Update friend-list.v1=.js

--- friends.tgblox.com/friend-list.v1=.js
+++ friends.tgblox.com/friend-list.v1=.js
@@ -3,7 +3,7 @@ document.write('            <img class="friends-pfp loading" src="images.tgblox.
 document.write('            <img class="friends-pfp loading" src="images.tgblox.com/friend4.png">');
 document.write('            <img class="friends-pfp loading" src="images.tgblox.com/friend2.png">');
 document.write('            <img class="friends-pfp loading" src="images.tgblox.com/friend3.png">');
-document.write('            <img class="friends-pfp loading" src="tr.tgnblx.com/broken-icon2.svg">');
+document.write('            <img class="friends-pfp loading" src="images.tgblox.com/friend5.png">');
 document.write('            <img class="friends-pfp loading" src="tr.tgnblx.com/broken-icon2.svg">');
 document.write('            <img class="friends-pfp loading" src="tr.tgnblx.com/broken-icon2.svg">');
 document.write('            <img class="friends-pfp loading" src="tr.tgnblx.com/broken-icon2.svg">');
